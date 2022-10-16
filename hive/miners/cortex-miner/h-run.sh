0b2343ab85e4a491ae0e15e5ca735ee62029b55f
catOs-mine <114361037+catOs-mine@users.noreply.github.com>
2022-10-16 19:47:27 +0300

    Delete hive/miners/cortex-miner directory

--- hive/miners/cortex-miner/h-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[[ `ps aux | grep "./cortex_miner" | grep -v grep | wc -l` != 0 ]] &&
-  echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
-  exit 1
-
-cd $MINER_DIR/$MINER_VER
-
-./cortex_miner `cat ${MINER_NAME}.conf` 2>&1 | tee --append $MINER_LOG_BASENAME.log
