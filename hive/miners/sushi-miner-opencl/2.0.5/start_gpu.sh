3fc780fed321a0c559b8ee98c89a14f4fce228ae
catOs-mine <114361037+catOs-mine@users.noreply.github.com>
2022-10-16 19:54:15 +0300

    Delete hive/miners/sushi-miner-opencl directory

--- hive/miners/sushi-miner-opencl/2.0.5/start_gpu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "------------------------START Miner----------------------"
-export UV_THREADPOOL_SIZE=24
-./sushi-miner-opencl
-echo "------------------------END Miner----------------------"
-echo "something went wrong or you exited"
