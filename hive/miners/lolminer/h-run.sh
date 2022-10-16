9d402361118da682d7bd003d2948c7d73b49b4f0
catOs-mine <114361037+catOs-mine@users.noreply.github.com>
2022-10-16 19:53:35 +0300

    Delete hive/miners/lolminer directory

--- hive/miners/lolminer/h-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd $MINER_DIR/$MINER_VER
-
-[[ ! -e ./lolminer.conf ]] && echo "No config file found, exiting" && exit 1
-
-lolMiner $(< lolminer.conf) | tee --append $MINER_LOG_BASENAME.log
