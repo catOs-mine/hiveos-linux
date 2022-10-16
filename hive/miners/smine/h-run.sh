b5d0edde41dc9926593a0a9aaa5d3183fb1540b6
catOs-mine <114361037+catOs-mine@users.noreply.github.com>
2022-10-16 19:54:07 +0300

    Delete hive/miners/smine directory

--- hive/miners/smine/h-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[[ `ps aux | grep "./smine" | grep -v grep | wc -l` != 0 ]] &&
-  echo -e "${RED}$MINER_NAME miner is already running${NOCOLOR}" &&
-  exit 1
-
-cd $MINER_DIR/$MINER_VER
-
-./smine `cat ${MINER_NAME}.conf` | tee --append $MINER_LOG_BASENAME.log
